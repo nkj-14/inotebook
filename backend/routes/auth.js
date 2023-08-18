822f3050d91e940afa16a4cda04e363254f6c901
nkj-14 <jadhav_2001ee21@iitp.ac.in>
2023-08-19 00:13:26 +0530

    Fetching all the notes and adding new note

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,9 +4,10 @@ const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'NehaIsGoodGirl'
+const JWT_SECRET = 'NehaIsGoodGirl';
+var fetchuser = require('../middleware/fetchuser');
 
-//Create a user using: post "/api/auth/createuser". No login required
+//Route 1 : Create a user using: post "/api/auth/createuser". No login required
 router.post('/createuser', [
     body('name','Enter a valid name').isLength({min: 3}),
     body('email','Enter a valid email').isEmail(),
@@ -42,4 +43,50 @@ router.post('/createuser', [
         res.status(500).send("Some error occured");
     }
 })
+
+//Route 2 : Authenticate a user using: post "/api/auth/login". No login required
+router.post('/login', [
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password cannot be blank').exists()
+], async (req, res)=>{
+    //If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors:errors.array()});
+    }
+    const {email,password} = req.body;
+    try{
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error: "Please try to login with correct credentials"});
+        }
+        const passwordCompare = await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error: "Please try to login with correct credentials"});
+        }
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data,JWT_SECRET);
+        res.json({authToken});
+    }catch(error){
+            console.error(error.message);
+            res.status(500).send("Internal server error");
+        }
+})
+
+//Route 3 : Get loggedin user details using: post "/api/auth/getuser". Login required
+router.post('/getuser', fetchuser, async (req, res)=>{
+    try{
+        userId = req.user.id;
+        const user = await User.findById(userId).select("-Password");
+        res.send(user);
+    } catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
+})
+
 module.exports = router
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser');
+const Notes = require('../models/Notes');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res)=>{
-    res.json([])
+//Route 1 : Get all the notes using: get "/api/notes/fetchallnotes". Login required
+router.get('/fetchallnotes', fetchuser, async (req, res)=>{
+    try{
+        const notes = await Notes.find({user: req.user.id});
+        res.json(notes);
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
+})
+
+//Route 2 : Add a new note using: post "/api/notes/addnote". Login required
+router.post('/addnote', fetchuser, [
+    body('title','Enter a valid title').isLength({min: 3}),
+    body('description','Description must be atleast 5 characters').isLength({min: 5})
+], async (req, res)=>{
+    try{
+        const {title, description, tag} = req.body;
+        //If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors:errors.array()});
+        }
+        const note = new Notes({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save();
+        res.json(savedNote);
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
 })
 module.exports = router
